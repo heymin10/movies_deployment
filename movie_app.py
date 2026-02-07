d563076d0664368b359d1c8ba849502b7a99d5ae
heymin10 <heymin10@hotmail.com>
2026-02-07 00:02:13 +0000

    realease version 1.3

--- movie_app.py
+++ movie_app.py
@@ -6,7 +6,7 @@ from google.cloud import firestore
 from google.oauth2 import service_account
 
 import json
-key_dict = json.loads(st.secrets["textkey"])
+key_dict = json.loads(st.secrets["text_key"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="movies")
 

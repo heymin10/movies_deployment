022373e43f85c62f572cb0023f8eab9d9ba80dec
heymin10 <heymin10@hotmail.com>
2026-02-07 00:29:00 +0000

    realease version 1.4

--- movie_app.py
+++ movie_app.py
@@ -8,7 +8,7 @@ from google.oauth2 import service_account
 import json
 key_dict = json.loads(st.secrets["text_key"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds, project="movies")
+db = firestore.Client(credentials=creds, project="movies-firebase")
 
 ##################################################################
 ################################### Definimos funciones auxiliares
